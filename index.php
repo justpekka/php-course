568c83163b1a6064d2cafe76fca302ea81edc13b
justpekka <ariefkhrisna631@gmail.com>
2022-02-07 17:08:34 +0700

    save this, 20%.

--- index.php
+++ index.php
@@ -8,9 +8,34 @@
     <title>RNG by Pekka</title>
   </head>
 
+  <?php
+    if($_SERVER['REQUEST_METHOD'] === "POST") {
+      $dices = $_REQUEST['dices'] != null ? $_REQUEST['dices'] : null;
+      $types = $_REQUEST['types'] != null ? $_REQUEST['types'] : null;
+      $i = 0;
+      $result = 0;
+      echo gettype($dices);
+      
+      // do {
+      //   echo $result . "\n";
+      //   // $result++;
+      //   $i == $dices;
+      // } while ($i++);
+
+      // for($i; $i<$dices; $i++) {
+      //   echo $result . "\n";
+      // }
+    };
+  ?>
+
   <body>
-    <form action="" method="get">
+    <form action="" method="post">
+      <input type="number" name="dices">
+      <input type="number" name="types">
+      <input type="submit" value="Submit">
     </form>
+
+    <span class="result"><?#php if($result) echo $result; ?></span>
   </body>
 
 </html>
